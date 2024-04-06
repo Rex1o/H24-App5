790d8a053d0e94e875545ad4fcf72fa25c977f6c
Rex1o <70145948+Rex1o@users.noreply.github.com>
2024-04-06 13:19:07 -0400

    Update Terminal.java

--- H24-App5/src/Terminal.java
+++ H24-App5/src/Terminal.java
@@ -4,18 +4,57 @@
  *  l'analyseur lexical
  */
 public class Terminal {
+    public enum Type {
+        Identificateur,
+        Nombre,
+        Addition,
+        Division,
+        Multiplication,
+        Soustraction,
+        ParentheseOuvrante,
+        ParentheseFermante
+    }
 
-    public String chaine;
 
-    public boolean isOperator;
+    private int position;
+    public int getPosition(){
+        return position;
+    }
 
+    private String chaine;
+    public String getChaine(){
+        return chaine;
+    }
+
+    private Type type;
+    public Type getType(){
+        return type;
+    }
 
     /** Un ou deux constructeurs (ou plus, si vous voulez)
      *   pour l'initalisation d'attributs
      */
-    public Terminal(String chaine) {   // arguments possibles
+    public Terminal(String chaine, int cursorPos, Type type) {
         this.chaine = chaine;
-        this.isOperator = chaine.equals("=");
+        this.type = type;
+        this.position = cursorPos;
     }
 
+    public Terminal(String chaine, int cursorPos , char operator) {
+        this.chaine = chaine;
+        this.type = getOperators(operator);
+        this.position = cursorPos;
+    }
+
+    private static Type getOperators (char c){
+        return switch (c) {
+            case '+' -> Type.Addition;
+            case '-' -> Type.Soustraction;
+            case '/' -> Type.Division;
+            case '*' -> Type.Multiplication;
+            case '(' -> Type.ParentheseOuvrante;
+            case ')' -> Type.ParentheseFermante;
+            default -> throw new RuntimeException(String.format("Caractère '%c' non supporté comme opérateur", c));
+        };
+    }
 }
